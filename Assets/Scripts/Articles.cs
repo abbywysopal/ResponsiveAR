79cc42060a738b53eeabf34c5a1d4bc50c73826d
abbywysopal <35503311+abbywysopal@users.noreply.github.com>
2022-04-21 20:47:41 -0700

    new application

---
+++ Assets/Scripts/Articles.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Articles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickedTitle1()
+    {
+        Debug.Log("log log log glo click click");
+    }
+
+    public void clickedTitle2()
+    {
+        Debug.Log("log log log glo click click");
+    }
+
+}
